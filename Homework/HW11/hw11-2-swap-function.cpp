0e96914e6b3e6a5c32117eed4b85a700389a40de
Jirphat Saetae <jirphat.aok@gmail.com>
2023-09-29 17:00:40 +0700

    1102

--- Homework/HW11/hw11-2-swap-function.cpp
+++ Homework/HW11/hw11-2-swap-function.cpp
@@ -22,4 +22,40 @@
         Name: Thane (31)
 
 */
+#include <stdio.h>
+#include <string>
 
+void swapage( int &age1, int &age2 ) {
+    int hold = age1; 
+    age1 = age2;     
+    age2 = hold ;     
+}
+int main () {
+    int age1, age2 ;
+    char name01[20] , name02[20] ;
+    
+    printf( "Input A\n" ) ;
+    printf( "Name :\n" ) ;
+    scanf( "%s",name01 ) ;
+    printf( "Age :\n" ) ;
+    scanf( "%d",&age1 ) ;
+
+    printf( "Input B\n" ) ;
+    printf( "Name :\n" ) ;
+    scanf( "%s",name02 ) ;
+    printf( "Age :\n" ) ;
+    scanf( "%d",&age2 ) ;
+    
+    printf( "** RESULT **\n" ) ;
+    printf( "Name: %s (%d)\n",name01,age1 ) ;
+    printf( "Name: %s (%d)\n",name02,age2 ) ;
+
+    swapage(age1, age2);
+    
+    printf( "** SWAP AGE **\n" ) ;
+    printf( "Name: %s (%d)\n",name01,age1 ) ;
+    printf( "Name: %s (%d)\n",name02,age2 ) ;
+
+
+    return 0 ;
+}
